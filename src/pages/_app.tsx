ee0820a11307c60ce534026d394b88ba1292fdb3
Michael Giuliana <michael.giuliana@gmail.com>
2020-12-15 10:24:10 -1000

    Add Fathom

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,31 @@
-import { App as AppComponent } from 'src/components';
 import 'src/styles/core.css';
 import 'src/styles/custom.css';
+import { App as AppComponent } from 'src/components';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
 
 function App({ Component, pageProps }) {
+    const router = useRouter();
+
+    useEffect(() => {
+        Fathom.load('EXCDTTBJ', {
+            includedDomains: [
+                'postmortemr.com',
+            ],
+        });
+
+        function onRouteChangeComplete() {
+            Fathom.trackPageview();
+        }
+
+        router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+        return () => {
+            router.events.off('routeChangeComplete', onRouteChangeComplete);
+        };
+      }, []);
+
     return (
         <AppComponent>
             <Component {...pageProps} />
